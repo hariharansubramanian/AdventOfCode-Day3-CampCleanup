8eeaf3f77bd1650719b78bdcd2444a136a736b91
Hariharan Subramanian <hari@flowinsights.com>
2022-12-04 21:42:37 +0100

    Calculating number of complete & partial overlaps of assigned camp sections

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Analyzing assigned sections");
+
+var assignedSections = File.ReadLines("assigned_sections.txt").ToList();
+
+// Puzzle 1
+var completelyOverlappingSectionsCount = 0;
+assignedSections.ForEach(pair =>
+{
+    var sections = pair.Split(',');
+    var firstElfSectionTag = sections[0].Split("-");
+    var secondElfSectionTag = sections[1].Split("-");
+
+    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), (int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1)).ToList();
+    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), (int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1)).ToList();
+
+    if (firstElfSections.All(s => secondElfSections.Contains(s)) || secondElfSections.All(s => firstElfSections.Contains(s)))
+    {
+        completelyOverlappingSectionsCount++;
+    }
+});
+
+Console.WriteLine($"There are {completelyOverlappingSectionsCount} assigned sections that are completely overlapping.");
+
+// Puzzle 2
+var partiallyOverlappingSectionsCount = 0;
+assignedSections.ForEach(pair =>
+{
+    var sections = pair.Split(',');
+    var firstElfSectionTag = sections[0].Split("-");
+    var secondElfSectionTag = sections[1].Split("-");
+
+    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), (int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1)).ToList();
+    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), (int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1)).ToList();
+
+    if (firstElfSections.Any(s => secondElfSections.Contains(s)) || secondElfSections.Any(s => firstElfSections.Contains(s)))
+    {
+        partiallyOverlappingSectionsCount++;
+    }
+});
+
+Console.WriteLine($"There are {partiallyOverlappingSectionsCount} assigned sections that are partially overlapping.");
+
+Console.WriteLine("Finished analyzing assigned camp sections.");
