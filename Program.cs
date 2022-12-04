be77f3f5a62ae3f2944354652950de342bd43f36
Hariharan Subramanian <hari@flowinsights.com>
2022-12-04 21:45:18 +0100

    Cleanup and final touches

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Analyzing assigned sections");
+﻿Console.WriteLine("Analyzing assigned sections");
 
 var assignedSections = File.ReadLines("assigned_sections.txt").ToList();
 
@@ -12,13 +10,10 @@ assignedSections.ForEach(pair =>
     var firstElfSectionTag = sections[0].Split("-");
     var secondElfSectionTag = sections[1].Split("-");
 
-    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), (int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1)).ToList();
-    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), (int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1)).ToList();
+    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1).ToList();
+    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1).ToList();
 
-    if (firstElfSections.All(s => secondElfSections.Contains(s)) || secondElfSections.All(s => firstElfSections.Contains(s)))
-    {
-        completelyOverlappingSectionsCount++;
-    }
+    if (firstElfSections.All(s => secondElfSections.Contains(s)) || secondElfSections.All(s => firstElfSections.Contains(s))) completelyOverlappingSectionsCount++;
 });
 
 Console.WriteLine($"There are {completelyOverlappingSectionsCount} assigned sections that are completely overlapping.");
@@ -31,13 +26,10 @@ assignedSections.ForEach(pair =>
     var firstElfSectionTag = sections[0].Split("-");
     var secondElfSectionTag = sections[1].Split("-");
 
-    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), (int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1)).ToList();
-    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), (int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1)).ToList();
+    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1).ToList();
+    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1).ToList();
 
-    if (firstElfSections.Any(s => secondElfSections.Contains(s)) || secondElfSections.Any(s => firstElfSections.Contains(s)))
-    {
-        partiallyOverlappingSectionsCount++;
-    }
+    if (firstElfSections.Any(s => secondElfSections.Contains(s)) || secondElfSections.Any(s => firstElfSections.Contains(s))) partiallyOverlappingSectionsCount++;
 });
 
 Console.WriteLine($"There are {partiallyOverlappingSectionsCount} assigned sections that are partially overlapping.");
