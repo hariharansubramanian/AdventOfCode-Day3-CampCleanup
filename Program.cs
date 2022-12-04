7f495f108945ba83c8822a597c90b84aa0c47f29
Hariharan Subramanian <hari@flowinsights.com>
2022-12-04 21:54:57 +0100

    Extracted common logic that extracts sections into GetAssignedSections()

--- Program.cs
+++ Program.cs
@@ -6,13 +6,7 @@ var assignedSections = File.ReadLines("assigned_sections.txt").ToList();
 var completelyOverlappingSectionsCount = 0;
 assignedSections.ForEach(pair =>
 {
-    var sections = pair.Split(',');
-    var firstElfSectionTag = sections[0].Split("-");
-    var secondElfSectionTag = sections[1].Split("-");
-
-    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1).ToList();
-    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1).ToList();
-
+    var (firstElfSections, secondElfSections) = GetAssignedSections(pair);
     if (firstElfSections.All(s => secondElfSections.Contains(s)) || secondElfSections.All(s => firstElfSections.Contains(s))) completelyOverlappingSectionsCount++;
 });
 
@@ -22,16 +16,23 @@ Console.WriteLine($"There are {completelyOverlappingSectionsCount} assigned sect
 var partiallyOverlappingSectionsCount = 0;
 assignedSections.ForEach(pair =>
 {
-    var sections = pair.Split(',');
-    var firstElfSectionTag = sections[0].Split("-");
-    var secondElfSectionTag = sections[1].Split("-");
-
-    var firstElfSections = Enumerable.Range(int.Parse(firstElfSectionTag[0]), int.Parse(firstElfSectionTag[1]) - int.Parse(firstElfSectionTag[0]) + 1).ToList();
-    var secondElfSections = Enumerable.Range(int.Parse(secondElfSectionTag[0]), int.Parse(secondElfSectionTag[1]) - int.Parse(secondElfSectionTag[0]) + 1).ToList();
-
+    var (firstElfSections, secondElfSections) = GetAssignedSections(pair);
     if (firstElfSections.Any(s => secondElfSections.Contains(s)) || secondElfSections.Any(s => firstElfSections.Contains(s))) partiallyOverlappingSectionsCount++;
 });
 
 Console.WriteLine($"There are {partiallyOverlappingSectionsCount} assigned sections that are partially overlapping.");
 
 Console.WriteLine("Finished analyzing assigned camp sections.");
+
+
+// Functions
+(List<int> firstElfSections, List<int> secondElfSections) GetAssignedSections(string assignmentPair)
+{
+    var sections = assignmentPair.Split(',');
+    var firstElfSectionTag = sections[0].Split("-").Select(int.Parse).ToArray();
+    var secondElfSectionTag = sections[1].Split("-").Select(int.Parse).ToArray();
+
+    var firstElfSections = Enumerable.Range(firstElfSectionTag[0], firstElfSectionTag[1] - firstElfSectionTag[0] + 1).ToList();
+    var secondElfSections = Enumerable.Range(secondElfSectionTag[0], secondElfSectionTag[1] - secondElfSectionTag[0] + 1).ToList();
+    return (firstElfSections, secondElfSections);
+}
