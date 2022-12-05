f4735a3e963e628ada967161b38818c6368ccade
Hariharan Subramanian <hari@flowinsights.com>
2022-12-05 22:22:53 +0100

    Refactored to use Ranges

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿Console.WriteLine("Analyzing assigned sections");
+﻿using System.Collections.Immutable;
+
+Console.WriteLine("Analyzing assigned sections");
 var assignedSections = File.ReadLines("assigned_sections.txt").ToList();
 
 // Puzzle 1
 var overlappingSectionsCount = 0;
 assignedSections.ForEach(pair =>
 {
-    var (startSection1, endSection1, startSection2, endSection2) = ParseSections(pair);
-    if (IsOverlapping(startSection1, endSection1, startSection2, endSection2)) overlappingSectionsCount++;
+    var sectionRanges = ParseSections(pair).ToImmutableArray();
+    if (IsOverlapping(sectionRanges[0], sectionRanges[1])) overlappingSectionsCount++;
 });
 Console.WriteLine($"There are {overlappingSectionsCount} assigned sections that are overlapping.");
 
@@ -15,32 +17,29 @@ Console.WriteLine($"There are {overlappingSectionsCount} assigned sections that 
 var intersectingSectionsCount = 0;
 assignedSections.ForEach(pair =>
 {
-    var (startSection1, endSection1, startSection2, endSection2) = ParseSections(pair);
-    if (IsIntersecting(startSection1, endSection1, startSection2, endSection2)) intersectingSectionsCount++;
+    var sectionRanges = ParseSections(pair).ToImmutableArray();
+    if (IsIntersecting(sectionRanges[0], sectionRanges[1])) intersectingSectionsCount++;
 });
 Console.WriteLine($"There are {intersectingSectionsCount} assigned sections that are partially overlapping.");
 
 // Functions
-(int startSection1, int endSection1, int startSection2, int endSection2) ParseSections(string assignmentPair)
+IEnumerable<Range> ParseSections(string assignmentPair)
 {
-    var sectionRanges = assignmentPair.Split(',');
-    var (startSection1, endSection1) = GetRange(sectionRanges[0]);
-    var (startSection2, endSection2) = GetRange(sectionRanges[1]);
-    return (startSection1, endSection1, startSection2, endSection2);
+    return assignmentPair.Split(',').Select(GetRange).ToList();
 }
 
-(int startSection, int endSection) GetRange(string assignmentPair)
+Range GetRange(string assignmentPair)
 {
     var sections = assignmentPair.Split('-').Select(int.Parse).ToArray();
-    return (sections[0], sections[1]);
+    return new Range(sections[0], sections[1]);
 }
 
-bool IsOverlapping(int start1, int end1, int start2, int end2)
+bool IsOverlapping(Range startRange, Range endRange)
 {
-    return start1 <= start2 && end1 >= end2 || start2 <= start1 && end2 >= end1;
+    return (startRange.Start.Value <= endRange.Start.Value && startRange.End.Value >= endRange.End.Value) || (endRange.Start.Value <= startRange.Start.Value && endRange.End.Value >= startRange.End.Value);
 }
 
-bool IsIntersecting(int start1, int end1, int start2, int end2)
+bool IsIntersecting(Range startRange, Range endRange)
 {
-    return start1 <= start2 && end1 >= start2 || start2 <= start1 && end2 >= start1;
+    return startRange.Start.Value <= endRange.End.Value && endRange.Start.Value <= startRange.End.Value;
 }
